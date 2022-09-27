217e3faf4d8ee38470f7de8a13be253c82291403
Natasha  Tatenda Chirombe <chirombenatashatatenda@gmail.com>
2022-09-27 19:24:40 +0300

    Update schema_based_on_diagram.sql
    -  Added FK keys and created diagnosis table.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,15 +1,18 @@
 CREATE TABLE "patients"(
     "id" INTEGER NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "date_of_birth" DATE NOT NULL
+    "date_of_birth" DATE NOT NULL,
+    PRIMARY KEY (id)
 );
 ALTER TABLE
     "patients" ADD PRIMARY KEY("id");
+    
 CREATE TABLE "medical_histories"(
     "id" INTEGER NOT NULL,
     "admitted_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "patient_id" INTEGER NOT NULL,
-    "status" VARCHAR(255) NOT NULL
+    "patient_id" INTEGER  NOT NULL  REFERENCES patients(id),,
+    "status" VARCHAR(255) NOT NULL,
+     PRIMARY KEY (id)
 );
 ALTER TABLE
     "medical_histories" ADD PRIMARY KEY("id");
@@ -18,7 +21,8 @@ CREATE TABLE "invoices"(
     "total_amount" INTEGER NOT NULL,
     "generated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "payed_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "medical_history_id" INTEGER NOT NULL
+    "medical_history_id" INTEGER NOT NULL REFERENCES medical_histories(id),
+    PRIMARY KEY (id)
 );
 ALTER TABLE
     "invoices" ADD PRIMARY KEY("id");
@@ -27,8 +31,15 @@ CREATE TABLE "invoice_items"(
     "unit_price" DECIMAL(8, 2) NOT NULL,
     "quantity" INTEGER NOT NULL,
     "total_price" DECIMAL(8, 2) NOT NULL,
-    "invoice_id" INTEGER NOT NULL,
-    "treatment_id" INTEGER NOT NULL
+    "invoice_id" INTEGER NOT NULL REFERENCES invoices(id),
+    "treatment_id" INTEGER NOT NULL REFERENCES treatments(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE diagnosis(
+      treat_id INTEGER NOT NULL  REFERENCES treatments(id),
+      med_hist_id INTEGER NOT NULL   REFERENCES medical_histories(id),
+      PRIMARY KEY( treat_id, med_hist_id) UNIQUE
 );
 ALTER TABLE
     "invoice_items" ADD PRIMARY KEY("id");
