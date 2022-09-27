c1b0015d099854a9dbefd0ea68fa1033b8af2a11
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-09-27 15:44:27 -0300

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,14 +5,14 @@ CREATE TABLE "patients"(
     "name" VARCHAR(255) NOT NULL,
     "date_of_birth" DATE NOT NULL,
    );
-   
+
 ALTER TABLE
     "patients" ADD PRIMARY KEY("id");
-    
+
 CREATE TABLE "medical_histories"(
     "id" INTEGER NOT NULL,
     "admitted_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-    "patient_id" INTEGER  NOT NULL  REFERENCES patients(id),,
+    "patient_id" INTEGER  NOT NULL  REFERENCES patients(id),
     "status" VARCHAR(255) NOT NULL,
     );
 ALTER TABLE
@@ -23,7 +23,6 @@ CREATE TABLE "invoices"(
     "generated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "payed_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "medical_history_id" INTEGER NOT NULL REFERENCES medical_histories(id),
-    
 );
 ALTER TABLE
     "invoices" ADD PRIMARY KEY("id");
@@ -48,21 +47,25 @@ CREATE TABLE "treatments"(
     "type" VARCHAR(255) NOT NULL,
     "name" VARCHAR(255) NOT NULL
 );
-ALTER TABLE
-    "treatments" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
-ALTER TABLE
-    "invoice_items" ADD CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
-ALTER TABLE
-    "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
-ALTER TABLE
-    "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
-    
---ADDING FK INDEXES
-CREATE INDEX patient_medic_history_id_idx ON medical_histories(patient_id);
-CREATE INDEX invoice_medic_history_invoice_idx ON invoices(medical_history_id);
-CREATE INDEX invoice_items_id_idx ON invoice_items(invoice_id);
-CREATE INDEX treatment_id_idx ON invoice_items(treatment_id);
-CREATE INDEX treatments_id_index_pres_treat_idx  ON prescribed_treatments(treatments_id);
-CREATE INDEX medi_history_prescribed_treatments_idx ON prescribed_treatments(medical_history_id);
+
+ALTER TABLE "treatments" ADD PRIMARY KEY("id");
+
+ALTER TABLE "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
+
+ALTER TABLE "invoice_items" ADD CONSTRAINT "invoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
+
+ALTER TABLE "invoice_items" ADD CONSTRAINT "invoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+
+ALTER TABLE "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+
+CREATE INDEX index_invoice_medic_history_invoice ON invoices(medical_history_id);
+
+CREATE INDEX index_id_patient_medic_history ON medical_histories(patient_id);
+
+CREATE INDEX index_invoice_items_id ON invoice_items(invoice_id);
+
+CREATE INDEX index_treatment_id ON invoice_items(treatment_id);
+
+CREATE INDEX index_medic_history_prescribed_treatments ON prescribed_treatments(medical_history_id);
+
+CREATE INDEX index_treatments_id_pres_treat  ON prescribed_treatments(treatments_id);
